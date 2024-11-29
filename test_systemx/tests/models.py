abd812b0327a5f4a3649e3926b98806f199aa69c
artem <gorelovartem470@gmail.com>
2024-11-29 16:55:35 +0300

    Added answers

--- test_systemx/tests/models.py
+++ test_systemx/tests/models.py
@@ -1,3 +1,30 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.template.defaultfilters import default
+
 
 # Create your models here.
+class Test(models.Model):
+    text = models.CharField(max_length=2048, default=" ")
+    mark = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(100)], default=100)
+
+
+
+class Question(models.Model):
+    class AnswerType(models.TextChoices):
+        CHOICE = "choice"
+        MULTICHOICE = "multichoice"
+        TEXT = "text"
+
+
+
+    test = models.ForeignKey(Test, on_delete=models.CASCADE, default=0)
+    text = models.CharField(max_length=2048)
+    answer_type = models.TextField(choices=AnswerType.choices, null=False, default=AnswerType.CHOICE)
+    right_answer_id = models.IntegerField() # TODO: Сделать нормальную систему выбора правильного ответа
+
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, default=0)
+    text = models.TextField(max_length=2048)
--- test_systemx/tests/views.py
+++ test_systemx/tests/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Test, Question, Answer
+
 
 # Create your views here.
 
 
 def test(response, test_id):
-    return HttpResponse(f"Viewing {test_id}")
+    test_obj = Test.objects.filter(id=test_id).first()
+
+    question_list = [i for i in Question.objects.filter(test__id=test_id)]
+    answer_list = {}
+    for i in question_list:
+        answer_list.update({i.id: [answer for answer in Answer.objects.filter(question__id=i.id)] })
+    return render(response, "test.html", context={"question_list": question_list, "test_obj": test_obj, "answer_list": answer_list})
